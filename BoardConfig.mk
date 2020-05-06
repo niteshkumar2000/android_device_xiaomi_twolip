52b6b084eea5ee437d201276c183d7f9a26a4481
AkhilmsAchu <akhilsree001@gmail.com>
2020-05-07 01:06:27 +0530

    twolip:Update vendor security patch from miui 10.0.3.0.PEKMIXM
    Signed-off-by: Niteshkumar <nitesh156200@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,7 +47,7 @@ BOARD_VENDORIMAGE_PARTITION_SIZE := 2147483648
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2018-11-01
+VENDOR_SECURITY_PATCH := 2020-02-05
 
 # Inherit the proprietary files
 -include vendor/xiaomi/twolip/BoardConfigVendor.mk
